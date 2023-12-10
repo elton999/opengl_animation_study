451b5ebcd46ed8130a6bf936ebac59fcfb9d0495
Elton Silva <eltoncarsilva73@gmail.com>
2023-12-10 20:15:53 -0300

    matrixs

--- AnimationProject/src/math/vec4.h
+++ AnimationProject/src/math/vec4.h
@@ -19,7 +19,7 @@ struct TVec4
 
 	inline TVec4<T>() : x((T)0), y((T)0), z((T)0), w((T)0) {}
 	inline TVec4<T>(T _x, T _y, T _z, T _w) : x(_x), y(_y), z(_z), w(_w) { }
-	inline TVec4<T>(T* fv) : x(fv[0]), y(fv[]), z(fv[2]), w(fv[3]) { }
+	inline TVec4<T>(T* fv) : x(fv[0]), y(fv[1]), z(fv[2]), w(fv[3]) { }
 };
 
 typedef TVec4<float> vec4;
