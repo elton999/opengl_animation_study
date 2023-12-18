1c4d6c8000257ee470e4fe9aa18c89ceafee7cf3
Elton Silva <eltoncarsilva73@gmail.com>
2023-12-18 20:54:50 -0300

    attribute class

---
+++ AnimationProject/src/Attribute.h
@@ -0,0 +1,33 @@
+#ifndef _ATTRIBUTE_H_
+#define _ATTRIBUTE_H_
+
+#include <vector>
+
+
+template<typename T>
+
+class Attribute
+{
+protected:
+	unsigned int mHandle;
+	unsigned int mCount;
+
+private:
+	Attribute(const Attribute& other);
+	Attribute& operator=(const Attribute& other);
+
+	void SetAttribPointer(unsigned int slot);
+
+public:
+	Attribute();
+	~Attribute();
+
+	void Set(T* inputArray, unsigned int arrayLenght);
+	void Set(std::vector<T>& input);
+	void BindTo(unsigned int slot);
+	void UnBindFrom(unsigned int slot);
+	unsigned int Count();
+	unsigned int GetHandle();
+};
+
+#endif // !_ATTRIBUTE_H_
