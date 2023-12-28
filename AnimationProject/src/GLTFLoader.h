69443e61ace8bbc3c9e72d0ff827e5742fb7f75a
Elton Silva <eltoncarsilva73@gmail.com>
2023-12-28 17:45:36 -0300

    glTFloader class fix

--- AnimationProject/src/GLTFLoader.h
+++ AnimationProject/src/GLTFLoader.h
@@ -2,8 +2,14 @@
 #define _H_GLTFLOADER_
 
 #include "cgltf.h"
+#include "animation/Pose.h"
+#include "animation/Clip.h"
 
 cgltf_data* LoadGLTFGFFile(const char* path);
 void FreeGLTFFile(cgltf_data* handle);
+
+Pose LoadRestPose(cgltf_data* data);
+std::vector<std::string> LoadJointNames(cgltf_data* data);
+std::vector<Clip> LoadAnimationClips(cgltf_data* data);
 
 #endif // !_H_GLTFLOADER_
